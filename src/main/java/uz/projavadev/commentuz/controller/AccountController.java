565bb749b0850af6924f05be522f55666586df26
temjashu8464 <temjashu8464@gmail.com>
2021-04-04 17:41:39 +0500

    bug fix

--- src/main/java/uz/projavadev/commentuz/controller/AccountController.java
+++ src/main/java/uz/projavadev/commentuz/controller/AccountController.java
@@ -6,6 +6,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import uz.projavadev.commentuz.dto.UserDto;
 import uz.projavadev.commentuz.entity.User;
 import uz.projavadev.commentuz.exception.UserNotFoundException;
 import uz.projavadev.commentuz.repository.UserRepository;
@@ -25,7 +26,7 @@ public class AccountController {
     public ResponseEntity getAccount() {
         String login = currentUser().get().getUsername();
         Optional<User> user = repository.findByUsername(login);
-        return ResponseEntity.ok(user);
+        return ResponseEntity.ok(UserDto.toDto(user.get()));
     }
 
 
